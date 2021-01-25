fc13c33525203ca0c89caf8000417356f9f18bed
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-25 20:24:08 +0100

    init

---
+++ inl-2/sales-system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sale_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
