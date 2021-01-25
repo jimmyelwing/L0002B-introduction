8f8316aa5044a6a3282c66d8e5074849abe24dac
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-25 20:29:31 +0100

    Added first question. How many sales persons?

--- inl-2/sales-system/Program.cs
+++ inl-2/sales-system/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace sale_system
 {
@@ -6,7 +7,22 @@ namespace sale_system
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej. Hur många säljare vill du registrera?");
+
+            var continuation = true; 
+            while (continuation)
+            {
+                var numberOfSaleAgents = Console.ReadLine();
+
+                if (!int.TryParse(numberOfSaleAgents, out int result))
+                {
+                    Console.WriteLine("Du har inte angett en siffra. Vänligen ange en siffra. ");
+                    continue;
+                }
+
+                continuation = false;
+            }
+            
         }
     }
 }
