187d6a1cb7ab6bc5d54632963f71184d2046d43e
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-25 20:42:09 +0100

    Created basic questions about seller

--- inl-2/sales-system/SalesSystemApplication.cs
+++ inl-2/sales-system/SalesSystemApplication.cs
@@ -12,7 +12,18 @@ namespace sales_system
 
             Console.WriteLine($"Du har angett att du vill registrera {numberOfSaleAgents} säljare.");
 
-
+            for (int i = 0; i < numberOfSaleAgents; i++)
+            {
+                Console.WriteLine($"\nSäljare nr. {i+1}");
+                Console.Write("Namn: ");
+                var name = Console.ReadLine();
+                Console.Write("Personnummer: ");
+                var identityNumber = Console.ReadLine();
+                Console.Write("Distrikt: ");
+                var district = Console.ReadLine();
+                Console.Write("Antal sålda artiklar: ");
+                var numberOfSoldItems = Console.ReadLine();
+            }
 
         }
 
