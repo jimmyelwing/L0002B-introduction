199fcfa082cbf674a0eb44106185271e9e9df51f
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-27 21:48:00 +0100

    Formatter in its own class

---
+++ inl-3/person-form/Helpers/Formatter.cs
@@ -0,0 +1,16 @@
+﻿namespace person_form.Helpers
+{
+    public static class Formatter
+    {
+        public static string FormatIdentityNumber(string identityNumber)
+        {
+            var identityNumberWithoutSeparator = identityNumber.Replace("-", "");
+
+            if (identityNumberWithoutSeparator.Length == 12)
+                return identityNumberWithoutSeparator.Substring(2);
+
+            return identityNumberWithoutSeparator;
+        }
+    }
+}
+    
