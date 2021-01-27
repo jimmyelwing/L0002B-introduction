b2d2ac0193ed86a3ab13d0806c73fbe85c8734cf
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-27 21:48:13 +0100

    Added calculation in its own class

---
+++ inl-3/person-form/Helpers/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace person_form.Helpers
+{
+    public static class Calculator
+    {
+        public static bool EvenNumber(int i)
+        {
+            return i % 2 == 0;
+        }
+    }
+}
