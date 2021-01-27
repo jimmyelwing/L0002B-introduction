59bf45e29053dff64a9dff2060e88a9b9f32c709
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-27 20:04:26 +0100

    Added person class and rename of form

---
+++ inl-3/person-form/Domain/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace person_form.Domain
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string IdentityNumber { get; set; }
+    }
+}
