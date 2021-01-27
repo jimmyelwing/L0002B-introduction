b5bc7b19b95b751f4b48b7464f3114176d8a138b
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-27 21:47:31 +0100

    Added a gender prop and calculate method

--- inl-3/person-form/Domain/Person.cs
+++ inl-3/person-form/Domain/Person.cs
@@ -1,11 +1,31 @@
-﻿namespace person_form.Domain
+﻿using person_form.Helpers;
+
+namespace person_form.Domain
 {
     public class Person
     {
-        public string FirstName { get; set; }
+        public Person(string firstName, string lastName, string identityNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            IdentityNumber = identityNumber;
+            Gender = CalculateGender(identityNumber);
+        }
 
-        public string LastName { get; set; }
+        public string FirstName { get; }
 
-        public string IdentityNumber { get; set; }
+        public string LastName { get; }
+
+        public string IdentityNumber { get; }
+
+        public Gender Gender { get; }
+
+        private Gender CalculateGender(string identityNumber)
+        {
+            return Calculator.EvenNumber(identityNumber[8])
+                ? Gender.Female 
+                : Gender.Male;
+        }
     }
 }
+
