bcf50760e17b8bedebac3aa66fa00cd92ed9d050
Jimmy Elwing <accounts@jimmyelwing.se>
2021-01-27 19:56:39 +0100

    Actual design files

--- inl-3/person-form/Form1.Designer.cs
+++ inl-3/person-form/Form1.Designer.cs
@@ -28,13 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.informationLabel = new System.Windows.Forms.Label();
+            this.identityNumberLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.identityNumberBox = new System.Windows.Forms.TextBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(110, 190);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 0;
+            this.sendButton.Text = "Skicka";
+            this.sendButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(110, 219);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 21);
+            this.cancelButton.TabIndex = 0;
+            this.cancelButton.Text = "Avbryt";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // informationLabel
+            // 
+            this.informationLabel.Location = new System.Drawing.Point(49, 35);
+            this.informationLabel.Name = "informationLabel";
+            this.informationLabel.Size = new System.Drawing.Size(181, 43);
+            this.informationLabel.TabIndex = 1;
+            this.informationLabel.Text = "Vänligen fyll i namn, efternamn och personnummer i formuläret nedan";
+            // 
+            // identityNumberLabel
+            // 
+            this.identityNumberLabel.AutoSize = true;
+            this.identityNumberLabel.Location = new System.Drawing.Point(15, 153);
+            this.identityNumberLabel.Name = "identityNumberLabel";
+            this.identityNumberLabel.Size = new System.Drawing.Size(89, 15);
+            this.identityNumberLabel.TabIndex = 1;
+            this.identityNumberLabel.Text = "Personnummer";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(42, 124);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(62, 15);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Efternamn";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(49, 95);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(55, 15);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "Förnamn";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(250, 35);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(232, 193);
+            this.resultBox.TabIndex = 2;
+            // 
+            // identityNumberBox
+            // 
+            this.identityNumberBox.Location = new System.Drawing.Point(110, 150);
+            this.identityNumberBox.Name = "identityNumberBox";
+            this.identityNumberBox.Size = new System.Drawing.Size(100, 23);
+            this.identityNumberBox.TabIndex = 2;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(110, 121);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(100, 23);
+            this.lastNameBox.TabIndex = 2;
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(110, 92);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(100, 23);
+            this.firstNameBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(515, 284);
+            this.Controls.Add(this.firstNameBox);
+            this.Controls.Add(this.lastNameBox);
+            this.Controls.Add(this.identityNumberBox);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.identityNumberLabel);
+            this.Controls.Add(this.informationLabel);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.sendButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label informationLabel;
+        private System.Windows.Forms.Label identityNumberLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.TextBox identityNumberBox;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.TextBox firstNameBox;
     }
 }
 
--- inl-3/person-form/Form1.cs
+++ inl-3/person-form/Form1.cs
@@ -17,5 +17,9 @@ namespace person_form
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
